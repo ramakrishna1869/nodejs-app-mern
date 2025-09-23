0fdb917c5b82f7e55c455b1b4e94e1fcc1459b60
ramakrishna1869 <ramakrishna.smilodon@gmail.com>
2025-09-23 14:07:24 +0530

    .env

--- mongoC.js
+++ mongoC.js
@@ -1,7 +1,7 @@
 import { MongoClient } from "mongodb";
 
 const password = encodeURIComponent(process.env.MONGO_PASSWORD.trim());
-const connectionString = `mongodb+srv://integrationninjas:${password}@devcluster.xf2gcci.mongodb.net/?retryWrites=true&w=majority`; // clustore url
+const connectionString = `mongodb+srv://ramakrishnasmilodon:rama1869@devcluster.b3x0aqg.mongodb.net/'
 const client = new MongoClient(connectionString);
 let conn;
 try {
