00634ba8c4106221c76b40c12c75054c7d21a2a2
ramakrishna1869 <ramakrishna.smilodon@gmail.com>
2025-09-23 14:15:05 +0530

    const connectionString = `mongodb+srv://ramakrishnasmilodon:rama1869@devcluster.b3x0aqg.mongodb.net/` updated mongo.c

--- mongoC.js
+++ mongoC.js
@@ -1,7 +1,7 @@
 import { MongoClient } from "mongodb";
 
 const password = encodeURIComponent(process.env.MONGO_PASSWORD.trim());
-const connectionString = `mongodb+srv://ramakrishnasmilodon:rama1869@devcluster.b3x0aqg.mongodb.net/'
+const connectionString = `mongodb+srv://ramakrishnasmilodon:rama1869@devcluster.b3x0aqg.mongodb.net/`;
 const client = new MongoClient(connectionString);
 let conn;
 try {
